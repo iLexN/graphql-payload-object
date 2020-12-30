6994c1e77c597149b0c22c91ab9a9a7ba352ef42
iLexN <ilex.job@gmail.com>
2020-12-30 18:05:09 +0800

    fix psalm

--- tests/build_in_function.php
+++ tests/build_in_function.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 
 namespace Ilex\GraphqlPayloadObject;
 
-function file_get_contents(string $path): mixed
+/**
+ * @param string $path
+ *
+ * @return string|false
+ */
+function file_get_contents(string $path): string|false
 {
     $fileName = basename($path);
 
-    if ($fileName === 'test.gql'){
+    if ($fileName === 'test.gql') {
         return \file_get_contents($path);
     }
 
